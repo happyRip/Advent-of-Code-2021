1cbbccee4384d266e71c19a507d9aca0b91df02f
Aleksander Borowski <aleksander.borowski@nokia.com>
2022-01-29 22:41:52 +0100

    day 1 groovy

---
+++ Day_1-Sonar_Sweep/Part_One/sonar_sweep.groovy
@@ -0,0 +1,21 @@
+import java.io.File
+
+class Main {
+    static void main(String[] args) {
+        def data = []
+        new File('../input.txt').eachLine {
+            line -> data.add(line)
+        }
+
+        def result = 0
+        data.eachWithIndex { v, i ->
+            if (i == 0) {
+                return
+            }
+            if (v > data[i-1]) {
+                result++
+            }
+        }
+        println result
+    }
+}
