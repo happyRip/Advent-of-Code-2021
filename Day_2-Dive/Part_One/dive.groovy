c837abe909ff16a86afa2a3d8ba5aa2e478b21e7
Aleksander Borowski <aleksander.borowski@nokia.com>
2022-01-30 15:42:24 +0100

    groovy day two

---
+++ Day_2-Dive/Part_One/dive.groovy
@@ -0,0 +1,27 @@
+import java.io.File
+
+class Dive {
+    static void main(String[] args) {
+        def data = []
+        new File('../input.txt').eachLine { line -> 
+            def (dir, val) = line.tokenize(' ')
+            data.add([dir, val as int])
+        }
+
+        def (depth, position) = [0, 0]
+        data.each { dir, val ->
+            switch (dir) {
+                case 'forward':
+                    position += val
+                    break
+                case 'up':
+                    depth -= val
+                    break
+                case 'down':
+                    depth += val
+                    break
+            }
+        }
+        println "${depth*position}"
+    }
+}
